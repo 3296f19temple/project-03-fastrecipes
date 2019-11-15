992425d2925eecc6f82b2fa22144b090ffc7520b
BigBadBoris <46152355+BigBadBoris@users.noreply.github.com>
2019-11-14 21:59:26 -0500

    scraper now imports all recipe info

--- scripts/BudgetBytesTest.py
+++ scripts/BudgetBytesTest.py
@@ -34,7 +34,50 @@ class Ingredient:
         self.name = "n/a"
 
 
-def get_recipe_name
+def print_recipe(recipe):
+    print("Name: ", recipe.name)
+    print("Prep Time: ", recipe.prep_time)
+    print("Cook Time: ", recipe.cook_time)
+    print("Total Time: ", recipe.total_time)
+    print("Servings: ", recipe.num_servings)
+    print("Ingredients: ")
+    for ingredient in recipe.ingredients:
+        print("\tName: ", ingredient.name)
+        print("\tAmount: ", ingredient.amount)
+        print("\tUnit: ", ingredient.unit)
+    print("Instructions: ")
+    for step in recipe.instructions:
+        print()
+        print(step)
+
+
+def get_recipe_name(soup, Recipe):
+    element = soup.select('.wprm-recipe-name')
+    Recipe.name = element[0].text
+
+
+def get_recipe_time(soup, Recipe):
+    # get prep time
+    element = soup.select('.wprm-recipe-prep_time')
+    Recipe.prep_time = element[0].text
+    # get cook time
+    element = soup.select('.wprm-recipe-cook_time')
+    Recipe.cook_time = element[0].text
+    # get total time
+    element = soup.select('.wprm-recipe-total_time')
+    Recipe.total_time = element[0].text
+
+
+def get_recipe_servings(soup, Recipe):
+    element = soup.select('.wprm-recipe-servings')
+    Recipe.num_servings = element[0].text
+
+
+def get_recipe_instructions(soup, Recipe):
+    elements = soup.select('.wprm-recipe-instruction')
+    for element in elements:
+        Recipe.instructions.append(element.text)
+    
 
 def get_recipe_ingredients(soup, Recipe):
     soup.select('.wprm-recipe-ingredient')
@@ -59,17 +102,20 @@ def get_recipe_ingredients(soup, Recipe):
         Recipe.ingredients.append(ingredient)
 
 
+
 # Test Code
 webpage = 'https://www.budgetbytes.com/sweet-potato-biscuits/'
 # fetch webpage
 res = requests.get(webpage)
 html_string = res.text
-
+# Get beautiful soup parser
 soup = bs4.BeautifulSoup(html_string, 'html.parser')
+# create new recipe object
 recipe = Recipe()
-get_ingredients(soup, recipe)
-for ingredient in recipe.ingredients:
-    print(ingredient.name)
-    print(ingredient.amount)
-    print(ingredient.unit)
-    print()
+# fill its contents
+get_recipe_ingredients(soup, recipe)
+get_recipe_instructions(soup, recipe)
+get_recipe_name(soup, recipe)
+get_recipe_time(soup, recipe)
+get_recipe_servings(soup, recipe)
+print_recipe(recipe)
