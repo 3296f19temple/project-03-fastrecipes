659c23b631021ea2b59721a3bc23f68c7969e783
BigBadBoris <46152355+BigBadBoris@users.noreply.github.com>
2019-11-15 14:54:23 -0500

    wrote serialize function for Recipes

--- scripts/BudgetBytesTest.py
+++ scripts/BudgetBytesTest.py
@@ -19,10 +19,10 @@ import requests
 class Recipe:
     def __init__(self):
         self.name = "n/a"
+        self.url = "n/a"
         self.category = "n/a"
         self.prep_time = "n/a"
         self.cook_time = "n/a"
-        self.total_time = "n/a"
         self.num_servings = "n/a"
         self.ingredients = []
         self.instructions = []
@@ -39,7 +39,7 @@ def print_recipe(recipe):
     print("Name: ", recipe.name)
     print("Prep Time: ", recipe.prep_time)
     print("Cook Time: ", recipe.cook_time)
-    print("Total Time: ", recipe.total_time)
+    #print("Total Time: ", recipe.total_time)
     print("Servings: ", recipe.num_servings)
     print("Ingredients: ")
     for ingredient in recipe.ingredients:
@@ -54,35 +54,48 @@ def print_recipe(recipe):
 
 def get_recipe_name(soup, Recipe):
     element = soup.select('.wprm-recipe-name')
+    if element == []:
+        return
     Recipe.name = element[0].text
 
 
 def get_recipe_time(soup, Recipe):
     # get prep time
     element = soup.select('.wprm-recipe-prep_time')
+    if element == []:
+        return
     Recipe.prep_time = element[0].text
     # get cook time
     element = soup.select('.wprm-recipe-cook_time')
+    if element == []:
+        return
     Recipe.cook_time = element[0].text
     # get total time
-    element = soup.select('.wprm-recipe-total_time')
-    Recipe.total_time = element[0].text
+    # element = soup.select('.wprm-recipe-total_time')
+    # if element == []:
+    #     return
+    # Recipe.total_time = element[0].text
 
 
 def get_recipe_servings(soup, Recipe):
     element = soup.select('.wprm-recipe-servings')
+    if element == []:
+        return
     Recipe.num_servings = element[0].text
 
 
 def get_recipe_instructions(soup, Recipe):
     elements = soup.select('.wprm-recipe-instruction')
+    if elements == []:
+        return
     for element in elements:
         Recipe.instructions.append(element.text)
     
 
 def get_recipe_ingredients(soup, Recipe):
-    soup.select('.wprm-recipe-ingredient')
     top_elements = soup.select('.wprm-recipe-ingredient')
+    if top_elements == []:
+        return
     for top_element in top_elements:
         children = top_element.children
         ingredient = Ingredient()
@@ -103,6 +116,47 @@ def get_recipe_ingredients(soup, Recipe):
         Recipe.ingredients.append(ingredient)
 
 
+def get_recipe_contents(soup, recipe):
+    get_recipe_ingredients(soup, recipe)
+    get_recipe_instructions(soup, recipe)
+    get_recipe_name(soup, recipe)
+    get_recipe_time(soup, recipe)
+    get_recipe_servings(soup, recipe)
+
+
+def write_recipe_to_file(fileName, recipe):
+    with open(fileName, 'w') as f:
+        f.write("name: ")
+        f.write(recipe.name)
+        f.write("\nurl: ")
+        f.write(recipe.url)
+        f.write("\ncategory: ")
+        f.write(recipe.category)
+        f.write("\nprep_time: ")
+        f.write(recipe.prep_time)
+        f.write("\ncook_time: ")
+        f.write(recipe.cook_time)
+        f.write("\nnum_servings: ")
+        f.write(recipe.num_servings)
+        f.write("\ningredients:")
+        for ingredient in recipe.ingredients:
+            f.write("\n\tname: ")
+            f.write(ingredient.name)
+            f.write("\n\tamount: ")
+            f.write(ingredient.amount)
+            f.write("\n\tunit: ")
+            f.write(ingredient.unit)
+        f.write("\n")
+        i = 0
+        for instruction in recipe.instructions:
+            f.write("step ")
+            f.write(str(i))
+            f.write(":\n")
+            f.write(instruction)
+            f.write("\n")
+            i = i + 1
+        f.write("\n---\n")
+
 
 # Test Code
 webpage = 'https://www.budgetbytes.com/sweet-potato-biscuits/'
@@ -114,9 +168,9 @@ soup = bs4.BeautifulSoup(html_string, 'html.parser')
 # create new recipe object
 recipe = Recipe()
 # fill its contents
-get_recipe_ingredients(soup, recipe)
-get_recipe_instructions(soup, recipe)
-get_recipe_name(soup, recipe)
-get_recipe_time(soup, recipe)
-get_recipe_servings(soup, recipe)
-print_recipe(recipe)
+get_recipe_contents(soup, recipe)
+recipe.url = webpage
+with open("recipe.yaml", 'w') as file:
+    yaml.dump(recipe.__dict__, file)
+    print("wrote file!")
+
